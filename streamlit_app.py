b8454083392cca2100229be393925036ef4d0f58
Robert Arcaleanu <112280290+robertarcaleanu@users.noreply.github.com>
2023-09-03 20:59:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -32,6 +34,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Print the dataframe 
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
